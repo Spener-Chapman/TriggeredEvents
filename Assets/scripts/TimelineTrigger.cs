88cd0ff1e02b54617aab3d8361295b52c7081588
Spener-Chapman <spencerchap@hotmail.com>
2021-11-24 09:21:24 -0400

    finished triggers
    finished and fixed the triggers based off of the feedback. Also added invisible walls in the doorways of the slamming doors to prevent being trapped

---
+++ Assets/scripts/TimelineTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineTrigger : MonoBehaviour
+{
+    public PlayableDirector timeline;
+
+    public bool OneTimeTrigger = false;
+
+    private void OnTriggerEnter(Collider other)
+    {        
+        timeline.Play();        
+
+        if (OneTimeTrigger == true)
+        {
+        Destroy(this.gameObject);
+        }        
+    }
+
+}
